c687b1aa8a74b158c95e460bf221f2b058ad4359
Noah Boeckmann <noahboeckmann@gmx.net>
2020-05-07 21:35:38 +0200

    Initial commit

---
+++ app/src/main/java/de/noahb/bikeservicetracker/MainActivity.java
@@ -0,0 +1,14 @@
+package de.noahb.bikeservicetracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
