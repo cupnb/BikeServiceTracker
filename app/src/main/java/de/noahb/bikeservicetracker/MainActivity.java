8b13cbbd54160e11e446d29ba7896a82987ec8d9
Noah Boeckmann <noahboeckmann@gmx.net>
2020-05-07 22:22:42 +0200

    Buttons added

--- app/src/main/java/de/noahb/bikeservicetracker/MainActivity.java
+++ app/src/main/java/de/noahb/bikeservicetracker/MainActivity.java
@@ -2,7 +2,11 @@ package de.noahb.bikeservicetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void newService(View v){
+        Intent i = new Intent();
+    }
+
 }
